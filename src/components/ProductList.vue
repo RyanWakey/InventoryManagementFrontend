b7bf9e46b6ef5ca8bbc102bba546ac902d9dfa4e
RyanWakey <wake52@outlook.com>
2023-12-02 05:07:41 +0000

    Created basic ProductList.vue

---
+++ src/components/ProductList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h1>Products</h1>
+      <ul>
+        <li v-for="product in products" :key="product.ProductID">
+          {{ product.Name }} - {{ product.Price }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        products: []
+      };
+    },
+    mounted() {
+      this.fetchProducts();
+    },
+    methods: {
+      fetchProducts() {
+        axios.get('http://your-api-url/products')
+          .then(response => {
+            this.products = response.data;
+          })
+          .catch(error => {
+            console.error('There was an error fetching the products:', error);
+          });
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* Add styles for your product list here */
+  </style>
+  
