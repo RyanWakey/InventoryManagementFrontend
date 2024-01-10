641657987247739ce72b4a97dbb408ef92a8b18c
RyanWakey <wake52@outlook.com>
2024-01-10 22:54:06 +0000

    testing cors between frontend and backend

---
+++ src/components/TestingCORS.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h1>Test API Call</h1>
+      <button @click="fetchProducts">Get Products</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TestApiCall',
+    methods: {
+      fetchProducts() {
+        fetch('http://localhost:18080/products')
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then(data => console.log(data))
+          .catch(error => console.error('Error:', error));
+      }
+    }
+  }
+  </script>
