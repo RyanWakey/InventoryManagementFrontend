6966472b6128b02a407cfc705bbd3254e81ffd0d
RyanWakey <wake52@outlook.com>
2024-01-11 02:51:13 +0000

    added template for the product page

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <h1>Products</h1>
+      <ProductForm @product-saved="fetchProducts"/>
+      <ProductList :products="products"/>
+    </div>
+  </template>
+  
+  <script>
+  import ProductForm from '@/components/ProductForm'
+  import ProductList from '@/components/ProductList'
+  
+  export default {
+    components: {
+      ProductForm,
+      ProductList
+    },
+    data() {
+      return {
+        products: []
+      }
+    },
+    methods: {
+      fetchProducts() {
+        // Fetch products from API and update `products` data property
+      }
+    },
+    created() {
+      this.fetchProducts();
+    }
+  }
+  </script>
+  
