4a9f8227420327f518f418e0ca961957cd2705ed
RyanWakey <wake52@outlook.com>
2024-01-11 04:27:37 +0000

    created product form default vue

---
+++ src/components/ProductForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <!-- Your form content will go here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProductForm',
+    // Your script goes here
+  }
+  </script>
+  
