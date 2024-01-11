dd0a9808ea2cdfc267a96a53bfaf0fcf70fba854
RyanWakey <wake52@outlook.com>
2024-01-11 04:58:29 +0000

    Added a basic layout component used accross all pages

---
+++ src/components/Layout.vue
@@ -0,0 +1,21 @@
+<!-- Layout.vue -->
+<template>
+    <div>
+      <NavBar />
+      <main>
+        <!-- This is where the current route's component will be rendered -->
+        <router-view></router-view>
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  import NavBar from './NavBar.vue';
+  
+  export default {
+    components: {
+      NavBar
+    }
+  }
+  </script>
+  
