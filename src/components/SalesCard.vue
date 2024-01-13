0a4b4aeb7ee773723710520dac792afd8ec1a5f3
RyanWakey <wake52@outlook.com>
2024-01-13 21:53:26 +0000

    Added fetch total revnue java script

--- src/components/SalesCard.vue
+++ src/components/SalesCard.vue
@@ -2,6 +2,9 @@
     <div class="sales-total">
         <h2> Total Sales Sold: {{ totalSales }}</h2>
     </div>
+    <div class="sales-revenue">
+        <h2> Total Revenue: £{{ totalRevenue }}</h2>
+    </div>
 </template>
 
 <script>
@@ -9,10 +12,12 @@ export default {
   data() {
     return {
       totalSales: 0,
+      totalRevenue: 0,
     };
   },
   mounted() {
     this.fetchTotalSales();
+    this.fetchTotalRevenue();
   },
   methods: {
     fetchTotalSales() {
@@ -25,9 +30,22 @@ export default {
       })
       .then(data => {
         console.log('Total sales data:', data);
-        this.totalSales = data; // Now it expects an object with a totalSales property
+        this.totalSales = data.totalSales; // Now it expects an object with a totalSales property
       })
       .catch(error => console.error('Error fetching total sales:', error));
+    },
+    fetchTotalRevenue() {
+      fetch("http://localhost:18080/sales/revenue").then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Total sales data: ', data);
+        this.totalRevenue = data.totalRevenue;
+      })
+      .catch(error => console.error('Error fetching total sales: ', error));
     }
   }
 }
@@ -38,17 +56,29 @@ export default {
 <style scoped>
 
 .sales-total {
-  background-color: #f0f0f0; /* Light grey background */
+  background-color: #f0f0f0; 
   padding: 16px;
   border-radius: 8px;
 }
 
 .sales-total h2 {
-  color: #1b1b1b; /* Dark grey text for less blackness */
+  color: #1b1b1b; 
   margin: 0;
   margin-left: 8px; /* Shift text to the right by 8px */
-  font-size: 16px; /* Smaller font size */
+  font-size: 16px; 
 }
 
+.sales-revenue {
+  background-color: #f0f0f0;
+  padding: 16px;
+  border-radius: 8px;
+}
+
+.sales-revenue h2 {
+  color: #1b1b1b; 
+  margin: 0;
+  margin-left: 8px; /* Shift text to the right by 8px */
+  font-size: 16px; 
+}
 
 </style>
