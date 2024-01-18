bfabdd914853b189d033b03c36d0566645769397
RyanWakey <wake52@outlook.com>
2024-01-18 05:35:31 +0000

    updated

--- src/components/TransactionsCard.vue
+++ src/components/TransactionsCard.vue
@@ -2,7 +2,7 @@
     <div class="transactions-dashboard-container">
         <div class="transactions-info-container">
             <div class="transactions-total"> 
-                <h2>Total Transactions made: {{ totalTransactions }}</h2>
+                <h2>Total Transactions made: </h2>
             </div>      
         </div>
     </div>
