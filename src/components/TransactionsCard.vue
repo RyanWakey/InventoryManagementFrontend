62ffe868e2368ff7ea6739f597f206fb7852d7c3
RyanWakey <wake52@outlook.com>
2024-01-18 04:23:11 +0000

    transactions card beginning

---
+++ src/components/TransactionsCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="transactions-dashboard-container">
+        <div class="transactions-info-container">
+            <div class="transactions-total"> 
+                <h2>Total Transactions made: {{ totalTransactions }}</h2>
+            </div>      
+        </div>
+    </div>
+  </template>
+  
