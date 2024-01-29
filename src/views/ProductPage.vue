9ee5a0f055b5b1e548a063320e1f0717427ab5f1
RyanWakey <wake52@outlook.com>
2024-01-29 06:42:52 +0000

    Select by category now works

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -107,6 +107,7 @@ export default {
   created() {
     this.fetchProducts();
     this.fetchCategories();
+    this.fetchAllProducts();
   },
 
   computed: {
@@ -123,6 +124,20 @@ export default {
       this.isLoading = true;
       try {
         const response = await axios.get(`http://localhost:18080/products/table-info`);
+        this.products = response.data; 
+        this.isLoading = false;
+      } catch (error) {
+        this.error = error;
+        this.isLoading = false;
+        console.error('There was an error fetching the products:', error);
+      }
+    },
+
+    async fetchAllProducts() {
+      this.isLoading = true;
+      try {
+        const response = await axios.get(`http://localhost:18080/products`);
+        console.log('Fetched Products:', response.data); // Log the fetched products
         this.products = response.data; 
         this.isLoading = false;
       } catch (error) {
