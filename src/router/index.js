fbb75e7a2b4908002bb8f62a6c3eb9213a03bc01
RyanWakey <wake52@outlook.com>
2024-01-31 05:10:32 +0000

    Added routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardPage from '../views/DashboardPage.vue'
 import ProductPage from '../views/ProductPage.vue' 
+import LoginPage from '@/views/LoginPage.vue'
+import RegisterPage from '@/views/RegisterPage.vue'
 
 const routes = [
   {
@@ -13,6 +15,16 @@ const routes = [
     name: 'Products',
     component: ProductPage
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginPage
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterPage
+  }
   // Define other routes as needed
 ]
 
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="register-container">
+    
+    </div>
+  </template>
+  
