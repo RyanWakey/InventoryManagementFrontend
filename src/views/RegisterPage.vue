8e8d7f46b5f4bb9e91b38165f438d10df97a29f7
RyanWakey <wake52@outlook.com>
2024-01-31 06:02:25 +0000

    register page foundation

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,6 +1,48 @@
 <template>
-    <div class="register-container">
-    
-    </div>
-  </template>
-  
+  <div class="registration-container">
+    <h1>Customer Registration</h1>
+    <form @submit.prevent="register">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="customer.email" required>
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="customer.password" required>
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="customer.password" required>
+      </div>
+
+
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      customer: {
+        email: '',
+        password: '',
+        name: '',
+        contactDetails: '',
+        address: '',
+        city: '',
+        country: '',
+      },
+    };
+  },
+  methods: {
+    register() {
+      // Implement registration logic here
+      // This will involve sending the customer data to the backend
+    },
+  },
+};
+</script>
