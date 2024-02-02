2211d410ba34369e1ece6dd0c5fb4d8eaf1efcbf
RyanWakey <wake52@outlook.com>
2024-02-02 04:49:11 +0000

    added nav bar routing based on logged in or not

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,5 +1,20 @@
 <template>
     <div class="profile-page">
-   
+    <button @click="logout">Logout</button>
     </div> 
-  </template>
+</template>
+
+<script>
+export default {
+
+  methods: {
+    logout() {
+      // Remove the token from localStorage
+      localStorage.removeItem('userToken');
+            
+      // Redirect the user to the login page or home page
+      this.$router.push('/dashboard');
+    }
+  }
+}
+</script>
