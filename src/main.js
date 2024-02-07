2c2e0361ed077239966f3cb56a0f60e4e3f97052
RyanWakey <wake52@outlook.com>
2024-02-07 05:32:46 +0000

    fix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'; // Import the store you just created
+import store from './reactive/store' // Import the store you just created
 
 const app = createApp(App)
 app.use(router) // Use Vue Router
